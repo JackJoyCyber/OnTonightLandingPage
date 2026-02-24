5adb85a3a4a3c7231e998140969a5a3f7cff8faa
Jack Joy <jackjoycybersec@gmail.com>
2026-02-24 00:39:46 -0500

    Add favicon and theme color meta tags

--- pages/_document.js
+++ pages/_document.js
@@ -1,15 +1,19 @@
-import { Html, Head, Main, NextScript } from 'next/document';
+import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
         <meta charSet="UTF-8" />
+        <link rel="icon" type="image/png" sizes="192x192" href="/icon-192.png" />
+        <link rel="icon" type="image/png" sizes="512x512" href="/icon-512.png" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+        <meta name="theme-color" content="#d4a373" />
       </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  );
+  )
 }
